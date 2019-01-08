a9dec9768dc600af3e2f3876c43fea2e9ca54d91
814476994@qq.com <814476994@qq.com>
2019-01-08 18:09:36 +0800

    '20190108'

--- 20190108/index.py
+++ 20190108/index.py
@@ -89,6 +89,50 @@ for key,value in d.items():
     print(key,value) # 1 a 2 2 3 3 4 4
 for item in d.items():
      print(item) # (1, 'a')(2, 2)(3, 3)('4', '4')
+msg = 'abcdefg'
+#for 循环
+for m in msg:
+    print(m) # a ---g
+#枚举 enumerate
+li = [1,2,3,4,5,6,'7']
+for i in enumerate(li):
+    print(i) #(0, 1)(1, 2)(2, 3)(3, 4)(4, 5)(5, 6)(6, '7')
+#range 随机
+for i in range(1,10,2):
+    print(i)
+#id id是内存地址
+print(id(1)) #8791266677792
+#is 比较的内存地址 ==比较的数值
+# python中pass的意义 1 空语句 do nothing 2 保证格式完整 3 保证语义完整
+#python 有5中标准的数据类型 Number(数字) String(字符串) List(列表) Tuple(元组) Dictionary(字典)
+# python支持不同的4中数字类型 int(有符号整型) long长整型(也可表示八进制和十六进制) float(浮点型) complex(复数)
+# 加号（+）是字符串连接运算符，星号（*）是重复操作
+# 元组相当于只读列表 元素不能二次赋值
+# repr(x) 将对象 x 转换为表达式字符串
+# chr(x) 将一个整数转换为一个字符
+# oct(x) 将一个整数转换为一个八进制字符串
+# hex(x) 将一个整数转换为一个十六进制字符串
+# ord(x) 将一个字符转换为它的整数值
+# unichr(x) 将一个整数转换为Unicode字符
+import math
+print(dir(math))
+print(dir(str))
+#可更改(mutable)与不可更改(immutable)对象
+#在python中，strings, tuples, 和numbers是不可更改的对象，而 list,dict 等则是可以修改的对象。
+#不可变类型：变量赋值a=5后再赋值a=10，这里实际是新生成一个int值对象10，再让a指向它，而5被丢弃，不是改变a的值，相当于新生成了a。
+#可变类型：变量赋值la=[1,2,3,4] 后再赋值la[2]=5则是将list la的第三个元素值更改，本身la没有动，只是其内部的一部分值被修改了。
+#python 函数的参数传递：
+#不可变类型：类似c++的值传递，如 整数、字符串、元组。如fun（a），传递的只是a的值，没有影响a对象本身。比如在 fun（a）内部修改 a 的值，只是修改另一个复制的对象，不会影响 a 本身。
+#可变类型：类似c++的引用传递，如 列表，字典。如fun（la），则是将la真正的传过去，修改后fun外部的la也会受影响
+#python中一切都是对象，严格意义我们不能说值传递还是引用传递，我们应该说传不可变对象和传可变对象。
+#匿名函数
+#python 使用 lambda 来创建匿名函数。
+#lambda只是一个表达式，函数体比def简单很多。
+#lambda的主体是一个表达式，而不是一个代码块。仅仅能在lambda表达式中封装有限的逻辑进去。
+#lambda函数拥有自己的命名空间，且不能访问自有参数列表之外或全局命名空间里的参数。
+#虽然lambda函数看起来只能写一行，却不等同于C或C++的内联函数，后者的目的是调用小函数时不占用栈内存从而增加运行效率。
+
+
 
 
 
